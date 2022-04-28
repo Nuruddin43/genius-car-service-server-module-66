8a40a42d9a673fa376849f44bb94d6ef8eb65f19
Nur Uddin <nuruddin46bd@gmail.com>
2022-04-28 22:39:04 +0600

    heroku deploy

--- _server_deploy.js
+++ _server_deploy.js
@@ -10,5 +10,10 @@ One time for your computer
 * For each project one time
 ----------------------
 * 1. heroku create
-* 2. 
+* 2. make sure you: git add . git commit . git push
+* 3. git push heroku main
+* 4. go to Heroku Dashboard > current Projects > settings > Reveal Configs Vars 
+* 5. copy paste config vars from your .env file
+* 6. make sure you have whitelisted all ip address to access mongodb
+* 7. 
 */
